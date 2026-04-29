488c9315e2f3b32efef0fbdf33c8697b126c0999
WinashAntsaw <winashantsaw4@gmail.com>
2026-04-29 17:05:39 -0400

    some basic tui setup

--- apps/client/client-main.cpp
+++ apps/client/client-main.cpp
@@ -10,10 +10,68 @@ using namespace net;
 #include <stdexcept>
 #include <thread>
 
+#include <curses.h>
+
 #define BUFF_SIZE 1024
 
 
+// basic initialization for the terminal user interface
+void init_tui(void) {
+	setlocale(LC_ALL, "");
+	initscr();
+	cbreak();
+	noecho();
+	nonl();
+	intrflush(stdscr, FALSE);
+	keypad(stdscr, TRUE);
+}
+
+
+// cleans up the terminal user interface
+void cleanup_tui(void) {
+	endwin();
+}
+
+// defines which window is currently in focus
+enum class Window_Focus : uint8_t {
+	USER_WINDOW = 1,
+	CHAT_HISTORY = 2,
+	CHAT_INPUT = 3
+};
+
 int main(void) {
+
+	// initialize the tui
+	init_tui();
+
+	// set which window should be in focus at the beginning
+	// for now, set the chat_input as the active window
+	Window_Focus focus = Window_Focus::CHAT_INPUT;
+	
+	// render loop
+	while (true) {
+		int ch = getch();
+
+
+		// q will simply exit the program
+		if (ch == 'q') {
+			break;
+
+		// cycle forward
+		} else if (ch == KEY_TAB) {
+			focus++;
+			focus %= 3;
+
+		// cycle backward
+		} else if (ch == KEY_BTAB) {
+			focus--;
+			focus = (static_cast<uint8_t>(focus) < 1)? 3 : focus;
+		}
+	}
+	
+	// cleanup the tui
+	cleanup_tui();
+
     // get the host:port from stdin
     std::string input;
     std::cout << "Enter the HOST:PORT\n";
